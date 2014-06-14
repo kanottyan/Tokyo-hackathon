1c67d4d8c869ce54a351458117f4b73d4b3bd1b4
Kanou Noboru <kanou@slogan.jp>
2014-06-15 00:55:44 +0900

    sqlデータのエラーを修正（すみませんでした）

--- Easy-stopping-server.sql
+++ Easy-stopping-server.sql
@@ -122,8 +122,8 @@ CREATE TABLE IF NOT EXISTS `users` (
   `age` int(150) NOT NULL,
   `email` varchar(50) NOT NULL,
   `address` varchar(100) NOT NULL,
-  'login_id'varchar(10) NOT NULL,
-  'login_password'varchar(10) NOT NULL,
+  `login_id` varchar(10) NOT NULL,
+  `login_password` varchar(10) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
